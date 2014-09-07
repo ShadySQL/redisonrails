16d51d2605a0e743e377b58b019ddadb86fca26a
Nathan LeClaire <nathan.leclaire@docker.com>
2014-09-06 17:32:54 -0700

    Mega commit of Redis on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = 'cecced51508592b8ba6dbac6e25c9850f01a16345f65a93ceac0f93ccd1ea0c71541e2ce3fdb1ab855c35c676925c9b132e0f160d19e048762a5dc5344005f44'
